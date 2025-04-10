7fc67bdbedf7eab1cd2b82f19035abb73d9e1a2f
Travis Lamberte <travis@lamberte.com>
2025-04-10 07:05:01 -0700

    refactoring styles

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -65,50 +65,6 @@ function Landing() {
 						</li>
 					</ul>
 				</div>
-
-				<div className='landing-text-container'>
-					<h1>
-						ALL SEATTLE GATE IS A SMALL HANDCRAFTED-TO-ORDER GATE COMPANY BASED
-						IN EDMONDS, SERVICING THE GREATER SEATTLE AREA. WE USE MODERN WOOD
-						AND METAL WORKING TECHNIQUES TO FIT YOU WITH THE PERFECT GATEWAY TO
-						YOUR OUTDOOR SPACE.
-					</h1>
-					<button>SEE GALLERY</button>
-				</div>
-				<div className='about-us-container'>
-					<div className='about-us-left'>
-						<h1 className='about-us-title'>About Us</h1>
-						<h1 className='about-us-text'>
-							At All Seattle Gate, we specialize in creating custom gates that
-							elevate your yard with beauty and craftsmanship. Founded by two
-							friends with expertise in woodworking and metal construction, we
-							design gates that are both functional and timeless. From wooden
-							gates for garden charm to sleek metal designs for a modern edge,
-							we turn your vision into a stunning reality.
-						</h1>
-						<h1 className='about-us-link'>Learn more about our story.</h1>
-					</div>
-					<div className='about-us-right'>
-						<img src={img5}></img>
-					</div>
-				</div>
-				<div className='FAQ-container'>
-					<div className='FAQ-left'>
-						<img src={img6}></img>
-					</div>
-					<div className='FAQ-right'>
-						<h1 className='FAQ-title'>Frequently Asked Questions</h1>
-						<h1 className='FAQ-body-text'>
-							We get a lot of questions about our products. Please read through
-							our <span>freqently asked Q&As</span> if you're curious.
-						</h1>
-					</div>
-				</div>
-				<div className='landing-text-container' id='tail-test'>
-					<h1>FIND YOUR PERFECT GATE</h1>
-					<button>CONTACT US</button>
-					<h1>IAN AND TRAVIS LOOK FORWARD TO IT!</h1>
-				</div>
 			</div>
 			<div className='grid-container'>
 				<div className='open-statement-container'>
@@ -137,9 +93,7 @@ function Landing() {
 					</div>
 				</div>
 				<div className='about-pic'>
-					<div>
-						<img src={img5}></img>
-					</div>
+					<img src={img5}></img>
 				</div>
 				<div className='FAQ-pic'>
 					<img src={img6}></img>
